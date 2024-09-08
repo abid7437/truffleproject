0b8e0cbe4267959817daf441520278bb8508bb60
Abid Hussain <ah6442@gmail.com>
2024-09-08 16:55:17 +0500

    demo page commit

--- pages/demo.js
+++ pages/demo.js
@@ -1,41 +1,108 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
-import { useState } from 'react'
+import { useState ,useRef,useEffect} from 'react'
+import WaveSurfer from 'wavesurfer.js';
+
+
 export default function Job() {
     const [activeIndex, setActiveIndex] = useState(1)
+    const waveformRef = useRef(null);
+    let wavesurfer;
+    const [playPause, setPlayPause] = useState();
+  
+    useEffect(() => {
+      wavesurfer = WaveSurfer.create({
+        container: waveformRef.current,
+        waveColor: "#ffffff",
+        progressColor: "#FF0000",
+        url: "assets/audio/sampleaudio.mp3",
+        dragToSeek: true,
+        width: "60vw",
+        hideScrollbar: true,
+        normalize: true,
+        barGap: 1,
+        height: 80,
+        barHeight: 20,
+        barRadius: 5,
+        barWidth: 3,
+      });
+  
+      wavesurfer.on("finish", () => {
+        console.log("song finished");
+      });
+  
+      wavesurfer.on("ready", () => {
+        console.log("Waveform is ready");
+      });
+      return () => {
+        wavesurfer.destroy();
+      };
+    }, []);
+    const handleStop = () => {
+      if (wavesurfer) {
+        wavesurfer.stop();
+      }
+    };
+    const handlePause = () => {
+      if (wavesurfer) {
+        wavesurfer.playPause();
+      }
+    };
+  
+    const handleSkipForward = () => {
+      if (wavesurfer) {
+        wavesurfer.skip(2);
+      }
+    };
+    const handleSkipBack = () => {
+      if (wavesurfer) {
+        wavesurfer.skip(-2);
+      }
+    };
+
+
     const handleOnClick = (index) => {
         setActiveIndex(index)
     }
     return (
         <>
             <Layout headerStyle={1} footerStyle={1} breadcrumbTitle={<>Try a <span>Demo</span></>}>
-            <div>
+                <div>
                     {/* faq-area */}
-                    
+
                     {/* faq-area-end */}
                     {/* help-area */}
                     <section className="help-area pb-50">
                         <div className="container">
-                          
+
                             <div className="row justify-content-center">
-                               
-                                           
-                                            <div className="col-md-12">
-                                                <div className="help-center-item">
-                                                    <div className="icon">
-                                                        <img id="uploadicon" src="assets/img/voice/upload.png"  alt="" />
-                                                    </div>
-                                                    <div className="content">
-                                                        <a >Upload File</a>
-                                                        <p>The best way to get answer faster.</p>
-                                                    </div>
-                                                </div>
-                                        
+
+
+                                <div className="col-md-12">
+                                    <div className="help-center-item">
+                                        <div className="icon">
+                                            <img id="uploadicon" src="assets/img/voice/upload.png" alt="" />
+                                        </div>
+                                        <div className="content">
+                                            <a >Upload File</a>
+                                            <p>The best way to get answer faster.</p>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
                     </section>
-
+                    <section className="job-area pb-50">
+                    <div className="container">
+                        <div className="job-item-wrap">
+                            <div className="job-item">
+                            <img id="uploadicon" src="assets/img/voice/playicon.png" alt="" /> <div ref={waveformRef} />
+                            </div>
+                        </div>
+                        
+</div>
+                    </section>
                     <section className="counter-area-three pb-130">
                         <div className="container">
                             <div className="row">
@@ -52,31 +119,31 @@ export default function Job() {
                                         <ul className="list-wrap">
                                             <li>
                                                 <div className="counter-item-three">
-                                                <div className="icon">
-                                                        <img id="uploadicon" src="assets/img/voice/playicon.png"  alt="" />
+                                                    <div className="icon">
+                                                        <img id="uploadicon" src="assets/img/voice/playicon.png" alt="" />
                                                     </div>
                                                 </div>
                                             </li>
                                             <li>
                                                 <div className="counter-item-three">
-                                                <div className="icon">
-                                                        <img id="uploadicon" src="assets/img/voice/download.png"  alt="" />
+                                                    <div className="icon">
+                                                        <img id="uploadicon" src="assets/img/voice/download.png" alt="" />
                                                     </div>
                                                 </div>
                                             </li>
                                             <li className="threeelist">
                                                 <div className="counter-item-three wavediv">
-                                                <img src="/assets/img/voice/WAVE.png"></img>
+                                                    <img src="/assets/img/voice/wavenew.png"></img>
                                                 </div>
                                             </li>
-                                           
+
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </section>
-
+                  
                     {/* help-area-end */}
                 </div>
             </Layout>
